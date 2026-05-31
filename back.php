e0250d45ccc0a79ee10af31fa155dc59423f147a
TokyMandresy <rakotoandrianarivony@gmail.com>
2026-05-31 19:37:03 +0300

    first commit

--- back.php
+++ back.php
@@ -1,17 +1,6 @@
 <?php
 
-	try {
-$conn = new PDO(
-    'mysql:host=localhost;dbname=database;charset=utf8',
-    'root',
-    ''
-);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-
-}catch(PDOException $e) {
-  die("Could not connect. " . $e->getMessage());
-} 
+	include('connexion.php');
 
 
 /*
