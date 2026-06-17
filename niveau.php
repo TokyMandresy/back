badb26bc2882523dbe166318e9ef73f11b5617a1
TokyMandresy <rakotoandrianarivony@gmail.com>
2026-06-17 15:53:31 +0300

    search

---
+++ niveau.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php
+			if ($_SESSION['type']==="admin") {
+
+	?>
+
+
+
+
+
+</body>
+</html>
