3b909ac696fb8dea5ef03f828306f6c2a01966cc
TokyMandresy <rakotoandrianarivony@gmail.com>
2026-06-19 11:32:09 +0300

    search

--- niveau.php
+++ niveau.php
@@ -1,5 +1,23 @@
 
+<?php
+try {
+$conn = new PDO(
+    'mysql:host=localhost;dbname=database;charset=utf8',
+    'root',
+    ''
+);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
 
+}catch(PDOException $e) {
+  die("Could not connect. " . $e->getMessage());
+} 
+session_start();
+
+if (@$_SESSION['autoriser']!=='oui') {
+	header('location:toky2.php'); }
+
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
