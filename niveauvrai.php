3a90d9d40f62b5fc29e95be84a55c19cda49a2bc
toki <rakotoandrianarivony@gmail.com>
2026-06-23 16:10:06 +0300

    search

--- niveauvrai.php
+++ niveauvrai.php
@@ -1,4 +1,37 @@
+ <?php 
+$c=0;
+ try {
+$conn = new PDO(
+    'mysql:host=localhost;dbname=database;charset=utf8',
+    'root',
+    ''
+);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
 
+}catch(PDOException $e) {
+  die("Could not connect. " . $e->getMessage());
+} 
+
+
+session_start();
+
+if (@$_SESSION['autoriser']!=='oui') {
+	header('location:toky2.php');
+
+		// code...
+	
+	// code...
+}
+if (isset($_POST['ok'])) {
+            @$nom=$_POST['no'];
+            @$nomp=$_POST['id'];
+                        $sql="UPDATE niveau set id=?";
+            $stmt= $conn->prepare($sql);
+            $stmt->execute(array($nomp));
+            
+          }
+?>
 <!DOCTYPE html>
 <html>
 <head>
